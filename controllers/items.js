63a30461405af0636044bc44aa2c1791d889e127
Parul Jain <pjain@LABA.PSWAS.LOCAL>
2023-06-26 14:38:22 -0700

    Added error handling code

--- controllers/items.js
+++ controllers/items.js
@@ -2,19 +2,28 @@ const Item = require("../models/Item");
 
 // Route to create a new item
 
-const createItem = (req, res)=>{
-    Item.create(req.body)
-    res.send("Item created!")
-}
+const createItem = async (req, res) => {
+  try {
+    const test = await Item.create(req.body);
+    if (Object.keys(req.body).length == 0) {
+      return res.status(400).send("Empty object!");
+    }
+    console.log(test);
+    res.send("Item created!");
+  } catch (error) {
+    console.log("Error creating the item", error);
+    res.status(400).send(error);
+  }
+};
 
 // Route to get all items
 
 const getItem = async (req, res) => {
-    let foundItem = await Item.find()
-    res.json(foundItem)
-  };
+  let foundItem = await Item.find();
+  res.json(foundItem);
+};
 
-  module.exports = {
-    createItem,
-    getItem
-  }
+module.exports = {
+  createItem,
+  getItem,
+};
--- models/item.js
+++ models/item.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-
 // Define the schema for the item
-const itemSchema = new mongoose.Schema({
-    price: Number,
+const itemSchema = new mongoose.Schema(
+  {
+    price: {
+      type: Number,
+      required: true,
+    },
     inventory: Number,
     nextDelivery: Date,
     deliveryAmt: Number,
     name: String,
-},
-{timestamps: true}
+  },
+  { timestamps: true }
 );
 
 // Create the Item model using the schema
-const Item = mongoose.model('Item', itemSchema);
+const Item = mongoose.model("Item", itemSchema);
 
 // Export the Item model
 module.exports = Item;
